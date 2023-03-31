782475cb3d5ad2caa0971afd00ef4d70980cb736
Mariana Sayago <108595604+MarianaSayago@users.noreply.github.com>
2023-03-30 21:11:29 -0300

    Add files via upload

---
+++ ventaDeAutos/src/main/java/com/marianasayago/ventaDeAutos/Automoviles.java
@@ -0,0 +1,11 @@
+package com.marianasayago.ventaDeAutos;
+
+public class Automoviles {
+    int id;
+    String modelo;
+    String marca;
+    String motor;
+    String color;
+    String matricula;
+    int numeroDePuertas;
+}
